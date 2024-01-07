999ea828555c121c86d84d8bc0ae3c917c4c0fd3
Sam Perry <17242713+sdp8483@users.noreply.github.com>
2024-01-07 11:02:19 -0500

    adjust bed temp thermal protection settings

--- AM8 Frame Upgrade/firmware/Marlin-2.1.2.1/Marlin/Configuration_adv.h
+++ AM8 Frame Upgrade/firmware/Marlin-2.1.2.1/Marlin/Configuration_adv.h
@@ -328,13 +328,13 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD        60 // Seconds
+  #define THERMAL_PROTECTION_BED_PERIOD        90 // Seconds
   #define THERMAL_PROTECTION_BED_HYSTERESIS    10 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
-  #define WATCH_BED_TEMP_PERIOD                60 // Seconds
+  #define WATCH_BED_TEMP_PERIOD                90 // Seconds
   #define WATCH_BED_TEMP_INCREASE               2 // Degrees Celsius
 #endif
 
