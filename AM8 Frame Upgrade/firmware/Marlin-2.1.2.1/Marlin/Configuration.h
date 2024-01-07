d5d1506ed5e682c4e2757063ed997307b01d7a37
Sam Perry <17242713+sdp8483@users.noreply.github.com>
2024-01-07 10:49:43 -0500

    set z probe offset

--- AM8 Frame Upgrade/firmware/Marlin-2.1.2.1/Marlin/Configuration.h
+++ AM8 Frame Upgrade/firmware/Marlin-2.1.2.1/Marlin/Configuration.h
@@ -1483,7 +1483,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 35, 20, 1.125 }
+#define NOZZLE_TO_PROBE_OFFSET { 35, 20, -1.125 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
